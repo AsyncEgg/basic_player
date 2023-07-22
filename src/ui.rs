b59b2a15dc321fbd291b6311d118ecbaf6a077fb
Egg55 <emilianomilanog@gmail.com>
2023-07-22 15:55:53 -0600

    better ui preformance

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp,
     error::Error,
     fs::File,
     io::{self, Stdout, Write},
@@ -12,13 +13,12 @@ use crossterm::{
 };
 use id3::{Tag, TagLike};
 use ratatui::{
-    prelude::{Alignment, Backend, Constraint, CrosstermBackend, Direction, Layout},
+    prelude::{Alignment, Backend, Constraint, CrosstermBackend, Direction, Layout, Rect},
     style::{Color, Style},
-    widgets::{Block, Borders},
+    widgets::{block::Position, Block, Borders},
     Frame, Terminal,
 };
-
-use crate::terminal_image::TerminalImage;
+use viuer::{print_from_file, Config};
 
 pub fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
     let mut stdout = io::stdout();
@@ -37,15 +37,14 @@ pub fn restore_terminal(
 
 struct App {
     tag: Tag,
-    image_path: String,
 }
 
 pub fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
     let path = "songs/Flamethrower.mp3";
-    let image_path = ".temp/album_cover.jpg".to_string();
+
     let tag = Tag::read_from_path(path).unwrap();
-    save_album_cover(&tag);
-    let app = App { tag, image_path };
+    save_album_cover(&tag)?;
+    let app = App { tag };
     loop {
         terminal.draw(|f| ui(f, &app))?;
         if event::poll(Duration::from_millis(250))? {
@@ -72,27 +71,67 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
         .borders(Borders::ALL)
         .border_style(Style::default().fg(Color::White));
 
-    f.render_widget(block, chunks[0]);
+    f.render_widget(block, center_area(chunks[0], 1));
 
     let inner_chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
-        .constraints([Constraint::Percentage(100), Constraint::Percentage(0)].as_ref())
+        .constraints([Constraint::Min(1), Constraint::Length(3)].as_ref())
         .split(chunks[0]);
 
-    let music_image_chunk = Layout::default()
-        .direction(Direction::Horizontal)
-        .margin(0)
-        .constraints([Constraint::Percentage(100)].as_ref())
-        .split(inner_chunks[0]);
+    render_image(".temp/album_cover.jpg", inner_chunks[0]);
 
-    let image_chunk = music_image_chunk[0];
+    let block = Block::default()
+        .title(app.tag.artist().unwrap())
+        .title_position(Position::Bottom)
+        .title_alignment(Alignment::Center);
 
-    let image = TerminalImage::new(&app.image_path);
-    f.render_widget(image, image_chunk)
+    f.render_widget(block, inner_chunks[1])
 }
 
-fn save_album_cover(tag: &Tag) {
+fn center_area(area: Rect, padding: u16) -> Rect {
+    let y = area.y;
+    let width = area.width;
+    let height = area.height;
+
+    let size = cmp::min(width, height);
+
+    // Ensure the image is rendered correctly by not rendering outside of box width
+    let image_width = match (size * 2) > width {
+        true => width,
+        false => (size * 2) + 1,
+    };
+
+    // Height is adjusted automatically
+    let image_height = size;
+
+    // Center image to half of width minus half of image width (and padding of 1)
+    let x = ((width / 2) - (image_width / 2)) + padding;
+    Rect {
+        x,
+        y,
+        width: image_width,
+        height: image_height,
+    }
+}
+
+//Using this with ratatui's custom widgit is just too slow and information that is unused is passed to this function so i decided to remove it for this function
+fn render_image(path: &str, area: Rect) {
+    let area = center_area(area, 2);
+    let conf = Config {
+        x: area.x,
+        y: area.y as i16,
+        width: Some(area.width as u32),
+        height: Some(area.height as u32),
+        ..Default::default()
+    };
+
+    if let Err(e) = print_from_file(path, &conf) {
+        eprintln!("Image printing failed: {}", e);
+    }
+}
+
+fn save_album_cover(tag: &Tag) -> Result<(), Box<dyn Error>> {
     // Look for APIC frame
     for picture in tag.pictures() {
         // Check if it is cover picture
@@ -100,11 +139,9 @@ fn save_album_cover(tag: &Tag) {
             // Set a name and path for your image file
             let image_path = ".temp/album_cover.jpg";
             // Create a new file and write the image data to it
-            let mut img_file = File::create(image_path).expect("Failed to create image file");
-            img_file
-                .write_all(&picture.data)
-                .expect("Failed to write data to file");
-            println!("Image saved to {}", image_path);
+            let mut img_file = File::create(image_path)?;
+            img_file.write_all(&picture.data)?;
         }
     }
+    Ok(())
 }
